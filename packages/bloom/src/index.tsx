f9186fafe6003bbf8d2cda69df07d2c3fed8217e
Guilherme Salviano <guilhermesalviano@Mac-mini-de-Guilherme.local>
2023-12-05 21:38:48 -0300

    refactor(bloom): adding custom configs

--- packages/bloom/src/index.tsx
+++ packages/bloom/src/index.tsx
@@ -15,17 +15,15 @@ export interface ButtonProps
 
 export const Button = ({
   className,
-  size = 'sm',
   variant = 'primary',
   ...rest
 }: ButtonProps) => {
   return (
     <button
       className={twMerge(
-        'flex items-center justify-center  rounded-full px-4  py-2 text-3xl',
+        'flex items-center justify-center  rounded-full px-4  py-2 text-md',
         className,
-        size === 'sm' ? 'h-9' : 'h-12',
-        variant === 'primary' && 'bg-slate-900 text-slate-100',
+        variant === 'primary' && 'bg-orange500 text-slate-100',
         variant === 'secondary' && 'bg-red-500 text-black',
         variant === 'tertiary' && 'bg-orange-500 text-green-400',
       )}
--- packages/bloom/tailwind.config.ts
+++ packages/bloom/tailwind.config.ts
@@ -1,10 +1,19 @@
 import { Config } from 'tailwindcss'
+import { colors, fontSizes, fontWeights, fonts, lineHeights, raddi, space } from '@koaris/tokens';
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors,
+      fontSize: fontSizes,
+      fontWeight: fontWeights,
+      fontFamily: fonts,
+      lineHeight: lineHeights,
+      raddi,
+      space
+    },
   },
   plugins: [],
 } satisfies Config
