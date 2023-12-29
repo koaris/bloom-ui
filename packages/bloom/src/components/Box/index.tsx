13f78cd3d04c2ee216da3ccae1fb83ef8f80aed0
Guilherme Salviano <guilhermesalviano@Mac-mini-de-Guilherme.local>
2023-12-29 03:07:01 -0300

    feat(Box): creating component

--- packages/bloom/src/components/Box/index.tsx
+++ packages/bloom/src/components/Box/index.tsx
@@ -1,24 +1,20 @@
 import { twMerge } from 'tailwind-merge'
 
-export interface TextProps {
+export interface BoxProps {
     children: React.ReactNode;
     color?: string;
     size?: 'xxs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl' | '6xl' | '7xl' | '8xl' | '9xl';
     variant?: 'p' | 'strong' | 'h1' | 'h2' | 'h3' | 'h4';
 }
 
-export const Text = ({ 
+export const Box = ({ 
     children,
-    color = 'neutral-800',
-    size = 'md',
-    variant = 'p'
-}: TextProps) => {
+}: BoxProps) => {
 
-    const Tag = variant;
 
     return (
-        <Tag className={twMerge(`text-${color} text-${size}`)}>
+        <div className={twMerge(`p-4 rounded-md bg-neutral-600 bottom-1 border-s-neutral-600`)}>
             {children}
-        </Tag>
+        </div>
     );
 };
--- packages/docs/src/stories/Text.stories.ts
+++ packages/docs/src/stories/Text.stories.ts
@@ -11,7 +11,7 @@ export default {
     size: {
       options: ['xxs', 'xs', 'sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl', '6xl', '7xl', '8xl', '9xl'],
       control: {
-        type: 'multi-select',
+        type: 'select',
       },
     }
   }
@@ -19,30 +19,22 @@ export default {
 
 export const Default: StoryObj<TextProps> = {}
 
-export const h1: StoryObj<TextProps> = {
+export const Title: StoryObj<TextProps> = {
   args: {
     variant: "h1",
-    size: "4xl"
-  }
-}
-
-export const h2: StoryObj<TextProps> = {
-  args: {
-    variant: "h2",
     size: "2xl"
   }
 }
 
-export const h3: StoryObj<TextProps> = {
+export const Subtitle: StoryObj<TextProps> = {
   args: {
-    variant: "h3",
+    variant: "h2",
     size: "xl"
   }
 }
 
-export const h4: StoryObj<TextProps> = {
+export const Strong: StoryObj<TextProps> = {
   args: {
-    variant: "h4",
-    size: "lg"
+    variant: "strong"
   }
 }
