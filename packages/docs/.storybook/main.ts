2189861a9e9ad5128fcc5505a19dd572a4b323eb
Guilherme Salviano <guilhermesalviano@Mac-mini-de-Guilherme.local>
2023-12-29 21:07:14 -0300

    fix(docs): subpath on production

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -28,5 +28,12 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === "PRODUCTION") {
+      config.base = '/bloom-ui/'
+    }
+
+    return config;
+  }
 };
 export default config;
